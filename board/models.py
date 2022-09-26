51924dde99cf8bcbe15a6e9f3bc77eeee0f5bff0
Blessian <blessian.dev@gmail.com>
2022-09-26 16:21:38 +0900

    feat: User모델, Board모델 추가

---
+++ board/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+from common.models import User
+
+
+# Create your models here.
+class Board(models.Model):
+    writer = models.PositiveIntegerField()
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+
+    hash_tag_regex = RegexValidator(regex=r'#([0-9a-zA-Zㅏ-ㅣㄱ-ㅎ가-힣\s]*),')
+    hash_tag = models.CharField(
+        max_length=256,
+        help_text='해시태그는 #로 시작되고 , 로 구분됩니다. ex) #맛집,#서울,#브런치 카페,#주말, … '
+    )
+
+    view_count = models.PositiveIntegerField(default=0)
+    like = models.ManyToManyField(User, related_name='likes', blank=True)
+    like_count = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
