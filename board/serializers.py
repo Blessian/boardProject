d1f4057c295ce46dc1ba2a2d346214faccfe694b
Blessian <blessian.dev@gmail.com>
2022-09-27 17:42:36 +0900

    feat: board serializer 추가

---
+++ board/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Board
+
+
+class BoardCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = ['writer', 'title', 'content', 'hash_tag']
+
+
+class BoardListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = ['id', 'writer', 'title', 'hash_tag', 'create_datetime', 'like_count', 'view_count']
+
+
+class BoardRetrieveUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = '__all__'
+        read_only_fields = ['id', 'writer', 'create_datetime', 'like', 'like_count', 'view_count', 'is_active']
+
+
+class BoardUpdateIsActiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = ['id', 'is_active']
